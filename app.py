60900bb61e7b2d787658c2f9a5066475c4141f40
RamKrishnaWavia <ramkrishna.appu@gmail.com>
2025-06-13 15:41:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -298,9 +298,9 @@ for label in sorted(df['Cluster'].unique()):
         st.markdown(f"**Total Orders:** {total_orders} | **Total Distance Travelled in Cluster:** {total_distance:.2f} km | **No. of Societies:** {len(cluster_df)} | **Max Leg Distance between the society:** {max_leg_distance:.2f} km | **DC to First Society:** {dc_to_first:.2f} km | **Total Distance via DC to last delivery point:** {dc_total_distance:.2f} km | **Total Round Trip (DC → Cluster → DC):** {round_trip_distance:.2f} km")
         st_folium(individual_map, width=725)
 
-    summary_df = pd.DataFrame(cluster_summary)
-    st.subheader("Cluster Summary")
-    st.dataframe(summary_df)
+        summary_df = pd.DataFrame(cluster_summary)
+        st.subheader("Cluster Summary")
+        st.dataframe(summary_df)
 
-    csv = summary_df.to_csv(index=False).encode('utf-8')
-    st.download_button("Download Cluster Summary CSV", data=csv, file_name="cluster_summary.csv")
+        csv = summary_df.to_csv(index=False).encode('utf-8')
+        st.download_button("Download Cluster Summary CSV", data=csv, file_name="cluster_summary.csv")
